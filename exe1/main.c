59119a96ff827bd2d52beba9c2bf568523d938f9
Vitor Katakura <7vitorgomez@gmail.com>
2025-03-29 13:25:08 -0300

    exercícios 1 e 2

--- exe1/main.c
+++ exe1/main.c
@@ -22,9 +22,22 @@ void adc_1_task(void *p) {
         result = adc_read();
         printf("voltage 1: %f V\n", result * conversion_factor);
 
-        // CÓDIGO AQUI
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
 
+void adc_2_task(void *p) {
+    adc_init();
+    adc_gpio_init(26);
 
+    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+    const float conversion_factor = 3.3f / (1 << 12);
+
+    uint16_t result;
+    while (1) {
+        adc_select_input(0); // Select ADC input 1 (GPIO27)
+        result = adc_read();
+        printf("voltage 2: %f V\n", result * conversion_factor);
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
@@ -35,7 +48,9 @@ int main() {
     printf("Start RTOS \n");
     adc_init();
 
-    xTaskCreate(adc_1_task, "LED_Task 1", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_1_task, "ADC_Task 1", 4095, NULL, 1, NULL);
+    xTaskCreate(adc_2_task, "ADC_Task 2", 4095, NULL, 1, NULL);
+
     vTaskStartScheduler();
 
     while (true) {
